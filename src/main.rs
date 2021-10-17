adab3e86e0353cd116d9a03e7cb2ffda21900ece
Cologler <skyoflw+github@gmail.com>
2021-10-17 15:52:35 +0800

    print with debug mode

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 
 fn main() {
     std::env::args().enumerate().for_each(|(idx, arg)| {
-        println!("{}: '{}'", idx, arg);
+        println!("{}: {:?}", idx, arg);
     });
     std::io::stdin().read_line(&mut String::new()).unwrap_or_default();
 }
